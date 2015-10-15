b368aba822dfc670498db8c6cccbac792366f41c
Enterthusiast <bernard.damien@gmail.com>
2015-10-16 00:37:15 +0200

    Made a working Cat Clicker with multiple cat again, with Knockout this time

--- js/app.js
+++ js/app.js
@@ -1,8 +1,61 @@
-var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('???');
+var initialCats = [
+	{
+		clickCount : 0,
+		name : 'LoL',
+		imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+		nicknames: [
+			{ nickname: "Poulet" },
+			{ nickname: "LeChat" },
+			{ nickname: "Coquinou" }
+		]
+	},
+	{
+		clickCount : 0,
+		name : 'Tiger',
+		imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+		imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+		nicknames: [
+			{ nickname: "Meow" }
+		]
+	},
+	{
+		clickCount : 0,
+		name : 'Scaredy',
+		imgSrc : 'img/22252709_010df3379e_z.jpg',
+		imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+		nicknames: [
+			{ nickname: "Scream" }
+		]
+	},
+	{
+		clickCount : 0,
+		name : 'Shadow',
+		imgSrc : 'img/1413379559_412a540d29_z.jpg',
+		imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+		nicknames: [
+			{ nickname: "Darky" }
+		]
+	},
+	{
+		clickCount : 0,
+		name : 'Sleepy',
+		imgSrc : 'img/9648464288_2516b35537_z.jpg',
+		imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+		nicknames: [
+			{ nickname: "Coma" }
+		]
+	}
+];
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+
+	this.nicknames = ko.observableArray(data.nicknames);
+
 	this.title = ko.computed(function(){
 		if(this.clickCount() >= 0 && this.clickCount() < 10) {
 				return 'Newborn';
@@ -25,9 +78,24 @@ var ViewModel = function() {
 			return "King of the Hill";
 		}
 	}, this);
+};
+
+var ViewModel = function() {
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+	this.changeCurrentCat = function(Cat) {
+		self.currentCat(Cat);
 	};
 };
 
